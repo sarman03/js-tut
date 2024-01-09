dc375add2cab64e73a32dccc19a45e0998163c9a
Sarmanpreet singh <131851235+sarman03@users.noreply.github.com>
2024-01-09 08:57:49 +0000

    changed

---
+++ 01_basics/math.js
@@ -0,0 +1,27 @@
+// +++++++++++++ Maths +++++++++++++++++++++++++++++
+
+console.log(Math);
+console.log(Math.abs(-4));
+//absolute value batata ye
+
+console.log(Math.round(4.6));
+//round off krta
+
+console.log(Math.ceil(4.2));
+//top value jese 4 se upar 5
+
+console.log(Math.floor(4.9));
+//niche wali value jese 4
+
+console.log(Math.min(4, 3, 6, 8));
+console.log(Math.max(4, 3, 6, 8));
+//min and maximum return krte
+
+console.log(Math.random());
+console.log((Math.random()*10) + 1);
+console.log(Math.floor(Math.random()*10) + 1);
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min)
